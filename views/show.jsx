b9a7fb697c1f76ced346af705cd923e915fea561
Michael Champlin <mchamplin@sbcglobal.net>
2022-03-26 13:22:02 -0700

    Update EDIT button location

--- views/show.jsx
+++ views/show.jsx
@@ -14,11 +14,11 @@ function Show ({bread, index}) {
           }
           have gluten.
         </p>
-        
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
         </form>
-        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
         <img src={bread.image} alt={bread.name} />
         <li><a href="/breads">Go home</a></li>
